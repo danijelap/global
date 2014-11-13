df72010d7d029a41252a62c1aab4f9b5c5bb7c6f
danijelap <bmddaca@gmail.com>
2014-11-13 17:20:39 +0100

    filter-namestenost

--- agencija/nekretnine/views.py
+++ agencija/nekretnine/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from nekretnine.models import Drzava, Objekat, DeoGrada
+from nekretnine.models import Drzava, Objekat, DeoGrada, TipObjekta, Namestenost
 
 
 def index(request):
@@ -9,12 +9,28 @@ def index(request):
 	return render(request, 'nekretnine/index.html', context)
 	
 def objekti(request):
-	deo_grada = request.GET.get('deo_grada', None)
-	if deo_grada is not None:
-		objekti = Objekat.objects.filter(deo_grada_id = deo_grada)
-	else:
-		objekti = Objekat.objects.all()
+	deo_grada = int(request.GET.get('deo_grada', 0))
+	tip_objekta = int(request.GET.get('tip_objekta', 0))
+	namestenost = int(request.GET.get('namestenost', 0))
+	filter_dictionary = {}
+	
+	if deo_grada != 0:
+		filter_dictionary['deo_grada_id'] = deo_grada
+	
+	if tip_objekta != 0:
+		filter_dictionary['tip_objekta_id'] = tip_objekta
+		
+	if namestenost != 0:
+		filter_dictionary['namestenost_id'] = namestenost
+		
+	objekti = Objekat.objects.filter(**filter_dictionary)
+	
+	tipovi_objekta = TipObjekta.objects.all();
 	delovi_grada = DeoGrada.objects.all();
-	context = {'objekti': objekti, 'delovi_grada': delovi_grada}
+	namestenosti = Namestenost.objects.all();
+	
+	context = {'objekti': objekti, 'delovi_grada': delovi_grada, 
+		'tipovi_objekta': tipovi_objekta, 'izabran_deo_grada': deo_grada, 
+		'izabran_tip_objekta': tip_objekta, 'izabrana_namestenost':namestenost}
 	return render(request, 'nekretnine/objekti.html', context)
 	
