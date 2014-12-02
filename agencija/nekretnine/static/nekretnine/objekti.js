5f5a726c3df492dc64a2e3539887e2908d0d9920
danijelap <bmddaca@gmail.com>
2014-12-02 22:14:33 +0100

    Load ponovo

--- agencija/nekretnine/static/nekretnine/objekti.js
+++ agencija/nekretnine/static/nekretnine/objekti.js
@@ -12,6 +12,6 @@ $(function() {
 		"-" + $( "#slider-range" ).slider( "values", 1 ) );
 });
 
-$(function vise() {
-	$("#prazan").load("/nekretnine/detalji?id_stana ={{ objekat.id }}");
+$(function vise(objekat_id) {
+	$("#prazan").load("/nekretnine/detalji?id_stana=" + objekat_id);
 });
