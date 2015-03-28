96797eef739701be73c16e4d75a549ae123ddf1b
Dejan Prokić <dprokic@gmail.com>
2015-03-28 17:08:22 +0100

    Production branch - DB changed to MySQL

--- agencija/agencija/settings.py
+++ agencija/agencija/settings.py
@@ -60,8 +60,12 @@ WSGI_APPLICATION = 'agencija.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'mysql.connector.django', 
+        'NAME': 'bim$nekretnine',
+        'USER': 'bim',
+        'PASSWORD': 'PASSWORD',
+        'HOST': 'mysql.server',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
