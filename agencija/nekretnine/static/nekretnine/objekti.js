35cdb9fbe6df607bab9a4febebce0b54aa418fc8
Dejan Prokić <dprokic@gmail.com>
2015-08-04 23:45:56 +0200

    Change library for image slider - jssor image slider ftw

--- agencija/nekretnine/static/nekretnine/objekti.js
+++ agencija/nekretnine/static/nekretnine/objekti.js
@@ -6,21 +6,34 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-64564624-1', 'auto');
 ga('send', 'pageview');
 
-window.sliderOptions=
-{
-	sliderId: "slider",
-	startSlide: 0,
-	effect: "13,17,13,13,5",
-	effectRandom: true,
-	pauseTime: 2600,
-	transitionTime: 500,
-	slices: 12,
-	boxes: 9,
-	hoverPause: 1,
-	autoAdvance: true,
-	captionOpacity: 1,
-	captionEffect: "fade",
-	m: false
+var _SlideshowTransitions = [
+	//Switch
+	{ $Duration: 500, x: 0.25, $Zoom: 1.5, $Easing: { $Left: $JssorEasing$.$EaseInWave, $Zoom: $JssorEasing$.$EaseInSine }, $Opacity: 2, $ZIndex: -10, $Brother: { $Duration: 700, x: -0.25, $Zoom: 1.5, $Easing: { $Left: $JssorEasing$.$EaseInWave, $Zoom: $JssorEasing$.$EaseInSine }, $Opacity: 2, $ZIndex: -10 } }
+];
+
+
+window.imageSliderOptions = {
+	$FillMode: 1,                                       //[Optional] The way to fill image in slide, 0 stretch, 1 contain (keep aspect ratio and put all inside slide), 2 cover (keep aspect ratio and cover whole slide), 4 actual size, 5 contain for large image, actual size for small image, default value is 0
+	$DragOrientation: 3,                                //[Optional] Orientation to drag slide, 0 no drag, 1 horizental, 2 vertical, 3 either, default value is 1 (Note that the $DragOrientation should be the same as $PlayOrientation when $DisplayPieces is greater than 1, or parking position is not 0)
+	$AutoPlay: true,                                    //[Optional] Whether to auto play, to enable slideshow, this option must be set to true, default value is false
+	$AutoPlayInterval: 2500,                            //[Optional] Interval (in milliseconds) to go for next slide since the previous stopped if the slider is auto playing, default value is 3000
+	$SlideshowOptions: {                                //[Optional] Options to specify and enable slideshow or not
+		$Class: $JssorSlideshowRunner$,                 //[Required] Class to create instance of slideshow
+		$Transitions: _SlideshowTransitions,            //[Required] An array of slideshow transitions to play slideshow
+		$TransitionsOrder: 1,                           //[Optional] The way to choose transition to play slide, 1 Sequence, 0 Random
+		$ShowLink: true                                    //[Optional] Whether to bring slide link on top of the slider when slideshow is running, default value is false
+	},
+
+	$BulletNavigatorOptions: {                                //[Optional] Options to specify and enable navigator or not
+		$Class: $JssorBulletNavigator$,                       //[Required] Class to create navigator instance
+		$ChanceToShow: 1,                               //[Required] 0 Never, 1 Mouse Over, 2 Always
+		$AutoCenter: 1,                                 //[Optional] Auto center navigator in parent container, 0 None, 1 Horizontal, 2 Vertical, 3 Both, default value is 0
+		$Steps: 1,                                      //[Optional] Steps to go for each navigation request, default value is 1
+		$Lanes: 1,                                      //[Optional] Specify lanes to arrange items, default value is 1
+		$SpacingX: 10,                                  //[Optional] Horizontal space between each item in pixel, default value is 0
+		$SpacingY: 10,                                  //[Optional] Vertical space between each item in pixel, default value is 0
+		$Orientation: 1                                 //[Optional] The orientation of the navigator, 1 horizontal, 2 vertical, default value is 1
+	}
 };
 window.isImageSliderLoaded = false;
 
@@ -203,7 +216,6 @@ $(function() {
 	$.cookie.json = true;
 	window.favourites = $.cookie('favourites');
 	if (typeof window.favourites != 'object') window.favourites = [];
-	console.log(window.favourites);
 
 	window.libFilter.initSlider();
 	init_document_click();
@@ -309,15 +321,11 @@ function init_document_click(){
 }
 
 function vise(objekat_id) {
+	if (window.isImageSliderLoaded) $("#slider1_container").remove();
 	$("#prazan").load("/detalji?id_stana=" + objekat_id, function() {
 		// initialize image slider when details are loaded
-		if (window.isImageSliderLoaded) { // if slider was already loaded then only reload images
-			window.imageSlider.reload();
-		} else { // create slider if it is not yet loaded
-			window.imageSlider = new mcImgSlider(window.sliderOptions);
-			window.isImageSliderLoaded = true;
-			window.imageSlider.reload();
-		}
+		window.imageSlider = new $JssorSlider$("slider1_container", window.imageSliderOptions);
+		window.isImageSliderLoaded = true;
 		ga('send', 'event', 'site_response', 'show', 'details');
 	});
 	window.location.hash = objekat_id;
@@ -331,7 +339,7 @@ function loadFlatList() {
 	Array.prototype.push.apply(favourite_flats, window.favourites);
 	object_id = parseInt(window.location.hash.replace('#', ''), 10);
 	if (!isNaN(object_id)) {
-		favourite_flats.push(object_id);
+		// favourite_flats.push(object_id);
 		vise(object_id);
 	}
 
